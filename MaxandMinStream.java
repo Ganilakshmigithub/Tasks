1d4642e0b7fd3f3d432625f3cb62fa8fc691417e
Bheri Gani Lakshmi <bheriganilakshmi@gmail.com>
2024-11-26 19:06:34 +0530

    Update MaxandMinStream.java

--- MaxandMinStream.java
+++ MaxandMinStream.java
@@ -4,7 +4,7 @@ public class MaxandMinStream {
     public static void main(String[] args) {
         List<Integer> numbers=List.of(2,5,1,3,56,21);
         int max=numbers.stream().max(Integer::compare).get();
-        int min=numbers.stream().min(Double::compare).get();
+        int min=numbers.stream().min(Integer::compare).get();
         System.out.println("Max number is "+max);
         System.out.println("min number is "+min);
 
