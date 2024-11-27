7f247104b24c6278cbd440df5ae1eec4c0928e15
Bheri Gani Lakshmi <bheriganilakshmi@gmail.com>
2024-11-27 13:52:17 +0530

    updated task

--- MaxAndMin.java
+++ MaxAndMin.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class MaxAndMin {
     public static void main(String[] args) {
         List<Integer> numbers=List.of(2,5,1,3,56,21);
-        int max=numbers.get(0);
+        int max=numbers.size()-1; // loop starts from first element even we are using last element as max
         int min=numbers.get(0);
         for(int nums:numbers){
             if(nums>max){
---
+++ Number.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+public class Number implements Comparable<Number> {
+    private int value;
+    Number(int value){
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+    
+    @Override
+    public int compareTo(Number o) {
+        return Integer.compare(this.value, o.value);
+
+    }
+}
+ class Main {
+    public static void main(String[] args) {
+        List<Number> numbers=List.of(
+        new Number(20),
+        new Number(30),
+        new Number(2),
+        new Number(42),
+        new Number(56),
+        new Number(1));
+        Number max=numbers.stream().max(Number::compareTo).get();
+        Number min=numbers.stream().min(Number::compareTo).get();
+        System.out.println("max number : "+max.getValue());
+        System.out.println("min number is: "+min.getValue());
+    }
+
+}
+
